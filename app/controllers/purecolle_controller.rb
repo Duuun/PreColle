8d08c494c8d7a6b1133c9b725be8f8eda29616cc
Daisuke Murakami <daimura10@gmail.com>
2018-04-09 23:31:47 +0900

    added crawler

--- app/controllers/purecolle_controller.rb
+++ app/controllers/purecolle_controller.rb
@@ -4,15 +4,24 @@ class PurecolleController < ApplicationController
   require "nokogiri"
 
   def index
-    uri = "http://anicobin.ldblog.jp/archives/cat_1137163.html"
+    url = "http://anicobin.ldblog.jp/archives/cat_1137163.html"
+    charset = nil
+
+    html = open(url) do |f|
+      charset = f.charset #文字種別を取得
+      f.read #htmlを読み込んで変数htmlに渡す
+    end
+
+    #htmlをパースしてオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    # @hoge = doc.css('img').map { |img| img.attr('src') }
+    #@nodes = doc.xpath('//*[(@class="ArticleFirstImageThumbnail")]//img')
+    @nodes = doc.css('.top-article-title a')
+    @images = doc.css('.ArticleFirstImageThumbnail a img').map {|img| img.attr('src')}
+
     # @doc = Nokogiri::HTML(open(uri), nil, "utf-8")
-    @doc = Nokogiri::HTML(open(uri), nil, "utf-8")
+    # @doc = Nokogiri::HTML(open(uri), nil, "utf-8")
+
   end
-
-  # def contact
-  # end
-
-  # def about
-  # end
 
 end
